eacd9fc99b5bb7c0a2000a4665147b547b015ebb
Shakti Swaroop Sahu <141706883+prosws2210@users.noreply.github.com>
2024-09-03 14:17:51 +0530

    few changes

--- FRONTEND/src/components/Login_Register/Register_body.jsx
+++ FRONTEND/src/components/Login_Register/Register_body.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import toast from "react-hot-toast";
 import {supabase} from "../../createClient";
 
