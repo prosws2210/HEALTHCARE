0c527b602ef2d856f8d9d430658ea604f3690971
Shakti Swaroop Sahu <141706883+prosws2210@users.noreply.github.com>
2024-09-03 15:54:36 +0530

    Update UserContext.jsx

--- FRONTEND/src/components/context/UserContext.jsx
+++ FRONTEND/src/components/context/UserContext.jsx
@@ -1,4 +1,3 @@
-import axios from "/axios";
 import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext({});
@@ -13,14 +12,17 @@ export function UserContextProvider({ children }) {
 	const getUser = async () => {
 		try {
 			const token = localStorage.getItem("token"); // Retrieve token from local storage
-			const res = await axios.get(
-				"http://localhost:5000/api/auth/refetch",
-				{
-					headers: { Authorization: `Bearer ${token}` },
-				}
-			);
-			console.log("Data recieved at UserContext.jsx getUser: ", res.data);
-			setUser(res.data);
+			const res = await fetch("http://localhost:5000/api/auth/refetch", {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			if (!res.ok) {
+				throw new Error("Failed to fetch user data");
+			}
+			const data = await res.json();
+			console.log("Data received at UserContext.jsx getUser: ", data);
+			setUser(data);
 		} catch (err) {
 			console.log("Error at UserContext.jsx getUser: ", err);
 		}
